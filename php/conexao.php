94996f47352ca75bb75dd66d7e7ed9384ccea334
proflobo <107000147+proflobo@users.noreply.github.com>
2024-10-07 08:59:51 -0300

    connection

--- php/conexao.php
+++ php/conexao.php
@@ -3,9 +3,9 @@
 subistitua as informações pelas do ser SQL Server
  ou PHPmyadmin */
 $servername = "localhost";
-$database = "u289605890_biblio";
-$username = "u289605890_biblio";//nome do USER de login no banco
-$password = "Controle23$";//senha do usuario do banco
+$database = "bancodedados";
+$username = "usuario";//nome do USER de login no banco
+$password = "senha123";//senha do usuario do banco
 //criando conexão com o banco
 $conecta = mysqli_connect($servername,$username,$password,$database);
 //verificando a conexão, se ta OK ou deu ERROr
